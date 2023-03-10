dcc4d703932307e7655235652f768dc6be480f8f
Ko0oT <serkokarjalo@gmail.com>
2023-03-10 17:34:49 +0300

    изменит дефолтный зум

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,5 @@ export const URL_MARKER_CURRENT =
   './media/pin-active.png';
 
 export const COMPANY_LOCATION: LatLngTuple = [61.779883, 34.357146];
-export const DEFAULT_ZOOM = 13;
+export const DEFAULT_ZOOM = 16;
 
